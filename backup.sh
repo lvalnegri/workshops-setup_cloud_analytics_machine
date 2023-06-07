e5c26ca6d4051207682c9e190b62f42f2d458af8
luca <l.valnegri@datamaps.co.uk>
2023-06-07 18:06:33 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # 0 4 * * * ~/cronjobs/backup.sh > ~/cronjobs/backup.log 2>&1
 
-sshpass -f ~/.priv/storage rsync -av --recursive -e 'ssh -p23' --exclude="osrm/*/*.fileIndex" --exclude="R_library/*" --exclude="python_libs/*" /usr/local/share/public u261064@u261064.your-storagebox.de:/home/backups/XXXXXX/
-sshpass -f ~/.priv/storage rsync -av --recursive -e 'ssh -p23' --exclude=".[!.]*" --exclude="software/*" --exclude="snap" /home/lucav u261064@u261064.your-storagebox.de:/home/backups/XXXXXX/
-sshpass -f ~/.priv/storage rsync -av --recursive -e 'ssh -p23' --exclude="media/*" /var/www/html u261064@u261064.your-storagebox.de:/home/backups/XXXXXX/
-sshpass -f ~/.priv/storage rsync -av --recursive -e 'ssh -p23' /srv/shiny-server u261064@u261064.your-storagebox.de:/home/backups/XXXXXX/
+--exclude="/*"
+
+sshpass -f ~/.priv/storage rsync -av --recursive -e 'ssh -p23' --exclude="osrm/*/*.fileIndex" --exclude="R_library/*" --exclude="python_libs/*" /usr/local/share/public u261064@u261064.your-storagebox.de:/home/backups/*******/
+sshpass -f ~/.priv/storage rsync -av --recursive -e 'ssh -p23' --exclude=".[!.]*" --exclude="node_modules/*" --exclude="software/*" --exclude="snap" /home/******* u261064@u261064.your-storagebox.de:/home/backups/*******/
+sshpass -f ~/.priv/storage rsync -av --recursive -e 'ssh -p23' --exclude="media/*" /var/www/html u261064@u261064.your-storagebox.de:/home/backups/*******/
+sshpass -f ~/.priv/storage rsync -av --recursive -e 'ssh -p23' /srv/shiny-server u261064@u261064.your-storagebox.de:/home/backups/*******/
