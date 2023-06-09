9ce7cc682cca6d7b607704cfbaf0a0fd1b73a5f6
luca <l.valnegri@datamaps.co.uk>
2023-06-10 00:49:18 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # 0 4 * * * ~/cronjobs/backup.sh > ~/cronjobs/backup.log 2>&1
 
---exclude="/*"
-
 sshpass -f ~/.priv/storage rsync -av --recursive -e 'ssh -p23' --exclude="osrm/*/*.fileIndex" --exclude="R_library/*" --exclude="python_libs/*" /usr/local/share/public u261064@u261064.your-storagebox.de:/home/backups/*******/
 sshpass -f ~/.priv/storage rsync -av --recursive -e 'ssh -p23' --exclude=".[!.]*" --exclude="node_modules/*" --exclude="software/*" --exclude="snap" /home/******* u261064@u261064.your-storagebox.de:/home/backups/*******/
 sshpass -f ~/.priv/storage rsync -av --recursive -e 'ssh -p23' --exclude="media/*" /var/www/html u261064@u261064.your-storagebox.de:/home/backups/*******/
